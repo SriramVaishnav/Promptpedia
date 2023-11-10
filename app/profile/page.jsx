e1ea82af6fd054d1e3c26e8fab2128764e082c48
Sriram Vaishnav R <sriramvaishnavr@gmail.com>
2023-11-10 22:41:51 +0530

    test commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,7 @@ const MyProfile = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const response = await fetch(`/api/prompt`);
       const data = await response.json();
 
       setMyPosts(data);
